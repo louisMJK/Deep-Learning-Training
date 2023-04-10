c3a0656fb2b612f7691fe18e7982f1767c8863c7
Yunlei (Louis) Lu <luyunlei99@gmail.com>
2023-04-10 17:22:49 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 ./train.sh 1 --model "vit_tiny" \
---batch_size 128 \
---opt adam --lr 0.001 \
---sched cosine --epochs 2 
+    --batch_size 128 \
+    --opt adam --lr 0.001 \
+    --sched cosine --epochs 2 
